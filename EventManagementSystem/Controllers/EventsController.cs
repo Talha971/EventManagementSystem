191b388f765fae663931c0336c1abae2233e6534
Talha971 <talhatahir971@gmail.com>
2025-04-04 21:56:17 +0500

    Controller added

---
+++ EventManagementSystem/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using EventManagementSystem.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public EventsController(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+    }
+}
