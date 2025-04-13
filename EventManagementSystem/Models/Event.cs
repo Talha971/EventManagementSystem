3d4afc42ab249fefb76418022b6ae61e34e41eb9
Talha971 <talhatahir971@gmail.com>
2025-04-13 22:56:04 +0500

    post method edit

--- EventManagementSystem/Models/Event.cs
+++ EventManagementSystem/Models/Event.cs
@@ -8,7 +8,7 @@
         public string Description { get; set; } = "";
         public DateTime Start { get; set; }
         public DateTime? End { get; set; }   
-        public bool Allday{ get; set; }
+        public bool AllDay{ get; set; }
         public DateTime CreatedAt{ get; set; }
 
 
